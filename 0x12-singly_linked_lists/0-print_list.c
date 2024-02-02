97881c8a8a3d2a2ec3b3eaca01643ad229ff15fe
Soufiane Arif <sarif@e3r3p14.1337.ma>
2024-02-02 12:49:09 +0100

    betty fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,15 @@
 size_t print_list(const list_t *h)
 {
 	unsigned int i = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n",h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
-	return(i);
+	return (i);
 }
