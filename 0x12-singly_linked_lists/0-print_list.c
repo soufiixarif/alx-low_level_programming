cf762a6fc176812eff9df202c1a97117eb34f465
Soufiane Arif <sarif@e3r3p14.1337.ma>
2024-02-02 12:39:48 +0100

    5

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,15 +8,15 @@
 
 size_t print_list(const list_t *h)
 {
-    unsigned int i = 0;
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n",h->len, h->str);
-        h = h->next;
-        i++;
-    }
-    return(i);
+	unsigned int i = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n",h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return(i);
 }
