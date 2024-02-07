aa4173fa1f6fd19307624866d230e551a70b1a23
Soufiane Arif <sarif@e1r9p2.1337.ma>
2024-02-07 09:42:31 +0100

    fixes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (!head || !*head)
+	if (head)
 		return ;
 	tmp = *head;
 	while (*head)
@@ -19,6 +19,6 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(tmp);
 	}
-	// free(*head);
+	free(*head);
 	*head = NULL;
 }
