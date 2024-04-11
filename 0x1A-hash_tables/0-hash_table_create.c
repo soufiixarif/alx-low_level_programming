60df56dd797d1553385f3cf8511a13f21cbc5d32
soufiane <soufiixarif@github.com>
2024-04-11 20:32:24 +0000

    lpr

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,10 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t) * size);
 	if (!table->array)
-	{
-		free(table);
 		return (NULL);
-	}
 	while (i < size)
 	{
 		table->array[i] = NULL;
